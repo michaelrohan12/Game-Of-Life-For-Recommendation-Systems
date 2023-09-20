17982a41bae48455eca003c882c82d95191003aa
michaelrscodes@gmail.com <michaelrscodes@gmail.com>
2023-09-20 10:49:01 +0530

    first commit

---
+++ game-of-life.py
@@ -0,0 +1,115 @@
+import time
+import pygame
+import numpy as np
+import json
+import os
+import random
+
+COLOR_BG = (10, 10, 10)
+COLOR_GRID = (40, 40, 40)
+COLOR_DIE_NEXT = (170, 170, 170)
+COLOR_ALIVE_NEXT = (255, 255, 255)
+
+
+def update(screen, cells, size, with_progress=False):
+    updated_cells = np.zeros((cells.shape[0], cells.shape[1]), dtype=np.dtype(
+        [('product_id', int), ('value', int)]))
+
+    for row in range(cells.shape[0]):
+        for col in range(cells.shape[1]):
+            updated_cells[row, col] = (cells[row, col]['product_id'], 0)
+
+    for row, col in np.ndindex(cells.shape):
+        neighborhood = cells[max(row - 1, 0):min(row + 2, cells.shape[0]),
+                             max(col - 1, 0):min(col + 2, cells.shape[1])]
+        alive = np.sum(neighborhood['value'])
+    
+        color = COLOR_BG if cells[row, col]['value'] == 0 else COLOR_ALIVE_NEXT
+
+        if cells[row, col]['value'] == 1:
+            if alive < 2 or alive > 3:
+                if with_progress:
+                    color = COLOR_DIE_NEXT
+            elif 2 <= alive <= 3:
+                updated_cells[row, col]['value'] = 1
+                if with_progress:
+                    color = COLOR_ALIVE_NEXT
+        else:
+            if alive == 3:
+                updated_cells[row, col]['value'] = 1
+                if with_progress:
+                    color = COLOR_ALIVE_NEXT
+
+        pygame.draw.rect(screen, color, (col * size,
+                         row * size, size - 1, size - 1))
+
+    return updated_cells
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+
+    with open(os.path.join('data_with_ids', 'Mobile.json'), 'r') as file:
+        sample_data = json.load(file)
+
+    grid_width = 80
+    grid_height = 60 
+
+    cells = np.zeros((grid_height, grid_width), dtype=np.dtype(
+        [('product_id', int), ('value', int)]))
+
+    product_ids = list(sample_data.keys())
+    for row in range(grid_height):
+        for col in range(grid_width):
+            cells[row, col] = (random.choice(product_ids), 0)
+
+    screen.fill(COLOR_GRID)
+    update(screen, cells, 10)
+    pygame.display.flip()
+    pygame.display.update()
+
+    running = False
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                # print(cells)
+                
+                chosen_product_ids = set()
+                for cell in cells:
+                    for value in cell:
+                        if value[1] != 0:
+                            chosen_product_ids.add(value[0])
+                
+                print("Chosen Product IDs:")
+                for product_id in chosen_product_ids:
+                    print(product_id)
+                
+                print(f"Total product ids for recommendation : {len(chosen_product_ids)}")
+                return
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    running = not running
+                    update(screen, cells, 10)
+                    pygame.display.update()
+
+            if pygame.mouse.get_pressed()[0]:
+                pos = pygame.mouse.get_pos()
+
+                cells[pos[1] // 10, pos[0] // 10]['value'] = 1
+                update(screen, cells, 10)
+                pygame.display.update()
+
+        screen.fill(COLOR_GRID)
+
+        if running:
+            cells = update(screen, cells, 10, with_progress=True)
+            pygame.display.update()
+
+        time.sleep(0.001)
+
+
+if __name__ == "__main__":
+    main()
